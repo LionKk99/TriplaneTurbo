b304d93bc3fe8a3d49559f501eb41d60e29902ea
LionKk99 <1185804084@qq.com>
2025-11-08 23:30:42 -0500

    Update download_eval_only to include sd1.5

--- scripts/prepare/download_eval_only.py
+++ scripts/prepare/download_eval_only.py
@@ -3,3 +3,8 @@ from diffusers import DiffusionPipeline
 repo_id = "stabilityai/stable-diffusion-2-1-base"
 pipeline = DiffusionPipeline.from_pretrained(repo_id, use_safetensors=True)
 pipeline.save_pretrained("./pretrained/stable-diffusion-2-1-base")
+
+from diffusers import StableDiffusionPipeline
+model_id = "sd-legacy/stable-diffusion-v1-5"
+pipe = StableDiffusionPipeline.from_pretrained(model_id, use_safetensors=True)
+pipe.save_pretrained("./pretrained/stable-diffusion-v1-5")
